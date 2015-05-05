896726e069dc51709f7cdf0eece32adf28223f30
Sam Bender <rednebmas@gmail.com>
2015-05-05 16:07:59 -0700

    Basic random tones and higher and lower view

--- main.js
+++ main.js
@@ -1,9 +1,51 @@
+var context, // audio context
+	referenceOscillator,
+	testingOscillator,
+	centDifference = 300,
+	level = 0;
+
 $(document).ready(function () {
-	context = new AudioContext;
-	oscillator = context.createOscillator();
-	oscillator.frequency.value = 200;
+	context = new (window.AudioContext || window.webkitAudioContext)();
 
-	oscillator.connect(context.destination);
+	// create oscillators
+	referenceOscillator = createOscillator(440);
+	testingOscillator = createOscillator(440);
 
-	oscillator.start(0);
+	play();
 });
+
+function play() {
+	var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+	testingOscillator = createOscillator(440 + plusOrMinus * diffInCentsFrom(440, centDifference));
+
+
+	// start
+	referenceOscillator.start(0);
+	testingOscillator.start(1.0);
+
+	// stop
+	referenceOscillator.stop(2);
+	testingOscillator.stop(2);
+}
+
+function createOscillator(freq) {
+	var oscillator = context.createOscillator();
+	oscillator.frequency.value = freq;
+
+	var gainNode = context.createGain();
+	gainNode.gain.value = 0.2;
+
+	/* Connections */
+	oscillator.connect(gainNode)
+	gainNode.connect(context.destination);
+
+	return oscillator;
+}
+
+function startTestingOscillator() {
+	testingOscillator.start(0);
+}
+
+function diffInCentsFrom(freq, cents) {
+	return freq * Math.pow(2, cents/1200) - freq;
+}
